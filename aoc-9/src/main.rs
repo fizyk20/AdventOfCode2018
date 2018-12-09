2c75615c51054ab095e80f8b08a7cb903f0f65b9
Bartłomiej Kamiński <fizyk20@gmail.com>
2018-12-09 13:10:29 +0100

    Day 9 part 2

--- aoc-9/src/main.rs
+++ aoc-9/src/main.rs
@@ -1,38 +1,84 @@
 const N_PLAYERS: usize = 477;
-const MAX_MARBLE: usize = 70851;
+
+use std::collections::HashMap;
+
+struct Marble {
+    prev_marble: usize,
+    next_marble: usize,
+}
 
 struct GameState {
-    current_marble_index: usize,
-    marbles: Vec<usize>,
+    current_marble: usize,
+    marbles: HashMap<usize, Marble>,
     points: [usize; N_PLAYERS],
     current_player: usize,
 }
 
 impl GameState {
-    fn new() -> Self {
+    fn new(max: usize) -> Self {
+        let mut marbles = HashMap::with_capacity(max + 1);
+        let _ = marbles.insert(
+            0,
+            Marble {
+                prev_marble: 0,
+                next_marble: 0,
+            },
+        );
         Self {
-            current_marble_index: 0,
-            marbles: vec![0],
+            current_marble: 0,
+            marbles,
             points: [0; N_PLAYERS],
             current_player: 0,
+        }
+    }
+
+    fn insert_marble_after_current(&mut self, marble: usize) {
+        let next_marble = self.marbles[&self.current_marble].next_marble;
+        self.marbles
+            .get_mut(&self.current_marble)
+            .unwrap()
+            .next_marble = marble;
+        self.marbles.get_mut(&next_marble).unwrap().prev_marble = marble;
+        self.marbles.insert(
+            marble,
+            Marble {
+                prev_marble: self.current_marble,
+                next_marble,
+            },
+        );
+        self.current_marble = marble;
+    }
+
+    fn remove_current_marble(&mut self) {
+        let prev_marble = self.marbles[&self.current_marble].prev_marble;
+        let next_marble = self.marbles[&self.current_marble].next_marble;
+        self.marbles.get_mut(&prev_marble).unwrap().next_marble = next_marble;
+        self.marbles.get_mut(&next_marble).unwrap().prev_marble = prev_marble;
+        let _ = self.marbles.remove(&self.current_marble);
+        self.current_marble = next_marble;
+    }
+
+    fn move_clockwise(&mut self, amount: usize) {
+        for _ in 0..amount {
+            self.current_marble = self.marbles[&self.current_marble].next_marble;
+        }
+    }
+
+    fn move_counterclockwise(&mut self, amount: usize) {
+        for _ in 0..amount {
+            self.current_marble = self.marbles[&self.current_marble].prev_marble;
         }
     }
 
     fn place_marble(&mut self, marble: usize) {
         if marble % 23 == 0 {
             self.points[self.current_player] += marble;
-            let to_remove = if self.current_marble_index >= 7 {
-                self.current_marble_index - 7
-            } else {
-                self.current_marble_index + self.marbles.len() - 7
-            };
-            let removed = self.marbles.remove(to_remove);
-            self.current_marble_index = to_remove % self.marbles.len();
-            self.points[self.current_player] += removed;
+            self.move_counterclockwise(7);
+            self.points[self.current_player] += self.current_marble;
+            self.remove_current_marble();
         } else {
-            let to_insert = (self.current_marble_index + 2) % self.marbles.len();
-            self.marbles.insert(to_insert, marble);
-            self.current_marble_index = to_insert;
+            self.move_clockwise(1);
+            self.insert_marble_after_current(marble);
         }
         self.current_player = (self.current_player + 1) % N_PLAYERS;
     }
@@ -42,12 +88,17 @@ impl GameState {
     }
 }
 
-fn main() {
-    let mut state = GameState::new();
+fn game_with_max_marble(max: usize) -> usize {
+    let mut state = GameState::new(max);
 
-    for marble in 1..=MAX_MARBLE {
+    for marble in 1..=max {
         state.place_marble(marble);
     }
 
-    println!("Part 1: {}", state.max_score());
+    state.max_score()
+}
+
+fn main() {
+    println!("Part 1: {}", game_with_max_marble(70851));
+    println!("Part 2: {}", game_with_max_marble(7085100));
 }
